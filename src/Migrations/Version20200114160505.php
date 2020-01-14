f5bee7065412327f723442bdd99c180d81e57a10
Lirith-Drasar <ma.rabouin@laposte.net>
2020-01-14 17:06:05 +0100

    version remise à zero pour future entité service

--- src/Migrations/Version20200114160505.php
+++ src/Migrations/Version20200114160505.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20200113164009 extends AbstractMigration
+final class Version20200114160505 extends AbstractMigration
 {
     public function getDescription() : string
     {
