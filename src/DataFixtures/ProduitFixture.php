05c83cf6ae1921f9c0c73793c8858abd49f50ea9
Lirith-Drasar <ma.rabouin@laposte.net>
2020-01-22 17:58:03 +0100

    Entity Produit avec nom et description + fixture + crud

---
+++ src/DataFixtures/ProduitFixture.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Produit;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\Persistence\ObjectManager;
+
+class ProduitFixture extends Fixture
+{
+    public function load(ObjectManager $manager)
+    {
+        $produit1 = new Produit();
+        $produit1->setNom('Nom du produit');
+        $produit1->setDescription('Description du produit et prix si besoin');
+        $manager->persist($produit1);
+
+        $manager->flush();
+    }
+}
